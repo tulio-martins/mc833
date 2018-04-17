16da50d1c107b71f223d428ab765cc79caa7bcab
nathyharumi <nathalia.kuromiya@gmail.com>
2018-04-17 15:49:29 -0300

    arrumando banco de disciplinas e pequenos consertos

--- projeto-1/menu-server-side.c
+++ projeto-1/menu-server-side.c
@@ -40,6 +40,22 @@ typedef struct {
 
 } Disciplina;
 
+/*funcoes*/
+
+void inicializandoDisciplinas(Disciplina** disc);
+/*Listar codigos das disciplinas com respectivos titulos*/
+void listDiscplines(int new_fd);
+/*Dado o codigo de uma disciplina, retornar a ementa*/
+void showDisciplineMenu(char id[], int new_fd, Disciplina disc[]);
+/*Dado o codigo de uma disciplina, retornar todas as informacoes da mesma*/
+void showDisciplineInfo (int new_fd);
+/*Retornar comentario da ultima aula da mesma disciplina*/
+void getComment(int new_fd, char id, Disciplina disc);
+/*Retorna o indice da disciplina com o id 'id' - retorna '-1' se nao encontrar*/
+int findDiscpline(char id, Disciplina disc[]);
+
+
+
 int main() {
 
     char option;
@@ -58,7 +74,7 @@ int main() {
 
     Disciplina disc[10];
 
-    inicializandoDisciplinas(disc);
+    inicializandoDisciplinas(&disc);
 
     /*Cria socket*/
     if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0))== -1) {
@@ -100,7 +116,7 @@ int main() {
            perror("accept");
            exit(1);
        } else {
-           printf("Servidor conseguiu conexão com o cliente %s\n", inet_ntoa(dest.sin_addr));
+           printf("Servidor conseguiu conexao com o cliente %s\n", inet_ntoa(dest.sin_addr));
            if ((pid = fork()) == 0){
                close(socket_fd);
                do {
@@ -118,7 +134,7 @@ int main() {
 
                        case DISCIPLINE_MENU:
 
-                          buffer = "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0";
+                          strcpy(buffer, "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0");
                           send(new_fd,buffer, strlen(buffer),0);
 
                           /*recebe a mensagem do cliente*/
@@ -136,7 +152,7 @@ int main() {
 
                        case DISCIPLINE_INFO:
 
-                          buffer = "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0";
+                          strcpy(buffer, "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0");
                           send(new_fd,buffer, strlen(buffer),0);
 
                           if ((num = recv(new_fd, client_disc_id, 6, 0))== -1 || num == 0) {
@@ -160,7 +176,7 @@ int main() {
 
                        case WRITE_COMMENT:
 
-                          buffer = "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0";
+                          strcpy(buffer, "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0");
                           send(new_fd,buffer, strlen(buffer),0);
 
                           if ((num = recv(new_fd, buffer, LINESIZE, 0))== -1 || num == 0) {
@@ -171,13 +187,13 @@ int main() {
                           } else {
 
                             /*Recebe texto */
-                            buffer = "Escreva o texto\0";
+                            strcpy(buffer, "Escreva o texto\0");
                             send(new_fd,buffer, strlen(buffer),0);
 
                             if ((num = recv(new_fd, text, TEXTSIZE, 0))== -1 || num == 0) {
                                /*Caso de erro, pode houver perda de conexao com o
                                 * cliente, portanto conexao deve ser terminada*/
-                               printf("Erro na recepcao de mensagem  terminando conexao\n");
+                               printf("Erro na recepcao de mensagem. Terminando conexao\n");
                                option = CONNECTION_CLOSED;
                             } else {
                               writeComment(text);
@@ -188,7 +204,7 @@ int main() {
 
                        case NEXT_CLASS_COMM:
 
-                          buffer = "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0";
+                          strcpy(buffer, "Escolha a discplina: MC833; MC102; MC536; MC750; MC358; MC458; MC558; MC658; MC346; MC886\0");
                           send(new_fd,buffer, strlen(buffer),0);
 
                           if ((num = recv(new_fd, buffer, LINESIZE, 0))== -1 || num == 0) {
@@ -203,10 +219,10 @@ int main() {
                           break;
 
                        case CONNECTION_CLOSED:
-                          printf("Erro na conexão - Cliente perdido\n");
+                          printf("Erro na conexao - Cliente perdido\n");
                           break;
                        case EXIT:
-                          printf("Conexão terminada\n");
+                          printf("Conexao terminada\n");
                           break;
                    }
 
@@ -226,79 +242,79 @@ int main() {
 
 /*Inicializa informacoes das disciplinas*/
 /*BEM INCOMPLETO AINDA :/ */
-void inicializandoDisciplinas(disc) {
-    disc[0].id = "MC833\0";
-    disc[0].titulo = "Programacao de Redes de Computadores\0";
-    disc[0].ementa = "Programacao utilizando diferentes tecnologias de comunicacao:\
-                      sockets, TCP e UDP, e chamada de metodo remoto.\0";
-    disc[0].sala_de_aula = "CC03\0";
-    disc[0].horario = "Quinta 10:00 a 12:00\0";
+void inicializandoDisciplinas(Disciplina** disc) {
+    strcpy(disc[0]->id, "MC833\0");
+    strcpy(disc[0]->titulo, "Programacao de Redes de Computadores\0");
+    strcpy(disc[0]->ementa, "Programacao utilizando diferentes tecnologias de comunicacao:\
+                      sockets, TCP e UDP, e chamada de metodo remoto.\0");
+    strcpy(disc[0]->sala_de_aula, "CC03\0");
+    strcpy(disc[0]->horario, "Quinta 10:00 a 12:00\0");
 
 
-    disc[1].id = "MC102\0";
-    disc[1].titulo = "Algoritmos e Programacao de Computadores\0";
-    disc[1].ementa = "Conceitos básicos de organização de computadores.\
-                      Construcao de algoritmos e sua representação em pseudocodigo\
+    strcpy(disc[1]->id, "MC102\0");
+    strcpy(disc[1]->titulo, "Algoritmos e Programacao de Computadores\0");
+    strcpy(disc[1]->ementa, "Conceitos basicos de organizacao de computadores.\
+                      Construcao de algoritmos e sua representacao em pseudocodigo\
                       e linguagens de alto nivel. Desenvolvimento sistematico e \
                       implementacao de programas. Estruturacao, depuracao, testes\
-                       e documentacao de programas. Resolucao de problemas.\0";
-    disc[1].sala_de_aula = "CC02";
-    disc[1].horario = "Sexta 14:00 a 16:00\0";
+                       e documentacao de programas. Resolucao de problemas.\0");
+    strcpy(disc[1]->sala_de_aula, "CC02\0");
+    strcpy(disc[1]->horario, "Sexta 14:00 a 16:00\0");
 
-    disc[2].id = "MC536\0";
-    disc[2].titulo = "Bancos de Dados: Teoria e Pratica\0";
-    disc[2].ementa = "Arquiteturas de sistemas de gerenciamento de bancos de dados.\
-                      Modelagem de dados: modelos conceituais e lógicos, incluindo o modelo relacional\
-                      e normalização. Álgebra relacional. Linguagens de definição e de manipulação de dados.\
-                      Otimização de consultas. Mecanismos de proteção, recuperação e segurança.\
-                      Controle de concorrência. Bancos de dados não relacionais.\
-                      Projeto e desenvolvimento de ferramentas e técnicas utilizadas na\
-                      solução de problemas de sistemas de informação, utilizando bancos de dados.\
-                      Modelagem, especificação, projeto e implementação de aplicações em sistemas de informação.\0";
-    disc[2].sala_de_aula = "CB02";
-    disc[2].horario = "Segunda 10:00 a 12:00; Quarta 10:00 a 12:00";
+    strcpy(disc[2]->id, "MC536\0");
+    strcpy(disc[2]->titulo, "Bancos de Dados: Teoria e Pratica\0");
+    strcpy(disc[2]->ementa, "Arquiteturas de sistemas de gerenciamento de bancos de dados.\
+                      Modelagem de dados: modelos conceituais e logicos, incluindo o modelo relacional\
+                      e normalizacao. algebra relacional. Linguagens de definicao e de manipulacao de dados.\
+                      Otimizacao de consultas. Mecanismos de protecao, recuperacao e seguranca.\
+                      Controle de concorrência. Bancos de dados nao relacionais.\
+                      Projeto e desenvolvimento de ferramentas e tecnicas utilizadas na\
+                      solucao de problemas de sistemas de informacao, utilizando bancos de dados.\
+                      Modelagem, especificacao, projeto e implementacao de aplicacoes em sistemas de informacao.\0");
+    strcpy(disc[2]->sala_de_aula, "CB02\0");
+    strcpy(disc[2]->horario, "Segunda 10:00 a 12:00; Quarta 10:00 a 12:00");
 
-    disc[0].id = "MC358\0";
-    disc[0].titulo = " 	Fundamentos Matemáticos da Computação\0";
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[3]->id, "MC358\0");
+    strcpy(disc[3]->titulo, "Fundamentos Matematicos da Computacao\0");
+    strcpy(disc[3]->ementa, "Conceitos basicos de matematica strcpy(discreta e de logica para computacao.Tecnicas de provas, inducao matematica. Relacoes e conceitos de teoria de grafos. Modelagem de problemas usando grafos.\0");
+    strcpy(disc[3]->sala_de_aula, "CB02\0");
+    strcpy(disc[3]->horario, "Terca 19:00 a 21:00; Quarta 19:00 a 21:00\0");
 
-    disc[0].id = "MC833\0";
-    disc[0].titulo = ;
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[4]->id, "MC346\0");
+    strcpy(disc[4]->titulo, "Programacao de Redes de Computadores\0");
+    strcpy(disc[4]->ementa, "Visao comparativa de paradigmas de programacao: programacao funcional, programacao logica e programacao rapida (prototipacao).\0");
+    strcpy(disc[4]->sala_de_aula, "CB12\0");
+    strcpy(disc[4]->horario, "Segunda 16:00 a 18:00; Quarta 16:00 a 18:00\0");
 
-    disc[0].id = "MC833\0";
-    disc[0].titulo = ;
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[5]->id, "MC750\0");
+    strcpy(disc[5]->titulo, "Construcao de Interfaces Homem-Computador\0");
+    strcpy(disc[5]->ementa, "Introducao a Interfaces Humano-Computador (IHC). Aspectos humanos. Aspectos tecnologicos. Processos, metodos e tecnicas de pesquisa, design e avaliacao. Ferramentas de suporte. Concepcao, modelagem e prototipacao de solucoes em interfaces de usuario.\0");
+    strcpy(disc[5]->sala_de_aula, "CB06\0");
+    strcpy(disc[5]->horario, "Terca 16:00 a 18:00; Quinta 16:00 a 18:00\0");
 
-    disc[0].id = "MC833\0";
-    disc[0].titulo = ;
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[6]->id, "MC458\0");
+    strcpy(disc[6]->titulo, "Projeto e Analise de Algoritmos I\0");
+    strcpy(disc[6]->ementa, "Tecnicas de projeto e analise de algoritmos. Ferramental Matematico para Analise de Algoritmos. Projeto de algoritmos por inducao. Busca, ordenacao e estatisticas de ordem. Programacao Dinâmica. Algoritmos Gulosos.\0");
+    strcpy(disc[6]->sala_de_aula, "PB18\0");
+    strcpy(disc[6]->horario, "Terca 10:00 a 12:00; Quinta 10:00 a 12:00\0");
 
-    disc[0].id = "MC833\0";
-    disc[0].titulo = ;
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[7]->id, "MC558\0");
+    strcpy(disc[7]->titulo, "Projeto e Analise de Algoritmos II\0");
+    strcpy(disc[7]->ementa, "Grafos: conceitos e algoritmos. Reducoes entre problemas. Programacao Linear. Classes de Problemas.\0");
+    strcpy(disc[7]->sala_de_aula, "CB17\0");
+    strcpy(disc[7]->horario, "Terca 16:00 a 18:00; Quinta 16:00 a 18:00\0" );
 
-    disc[0].id = "MC833\0";
-    disc[0].titulo = ;
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[8]->id, "MC658\0");
+    strcpy(disc[8]->titulo, "Projeto e Analise de Algoritmos III\0");
+    strcpy(disc[8]->ementa, "Tratamento de Problemas NP-dificeis: algoritmos exatos, algoritmos aproximados e algoritmos heuristicos.\0");
+    strcpy(disc[8]->sala_de_aula, "CB13\0");
+    strcpy(disc[8]->horario, "Segunda 19:00 a 21:00; Quarta 21:00 a 23:00\0");
 
-    disc[0].id = "MC833\0";
-    disc[0].titulo = ;
-    disc[0].ementa = ;
-    disc[0].sala_de_aula = ;
-    disc[0].horario = ;
+    strcpy(disc[9]->id, "MC886\0");
+    strcpy(disc[9]->titulo, "Aprendizado de Maquina\0");
+    strcpy(disc[9]->ementa, "Tecnicas de aprendizado de maquina estatistico para classificacao, agrupamento e deteccao de outliers.\0");
+    strcpy(disc[8]->sala_de_aula, "CB01\0");
+    strcpy(disc[8]->horario, "Segunda 19:00 a 21:00; Quarta 21:00 a 23:00");
 
 
 }
@@ -356,7 +372,7 @@ void getComment(int new_fd, char id, Disciplina disc) {
     send(new_fd, ERROR_MESSAGE, TEXTSIZE, 0);
   } else {
     send(new_fd, disc[i].comentario_ultima_aula, TEXTSIZE, 0);
-  }/
+  }
 }
 
 /*Retorna o indice da disciplina com o id 'id' - retorna '-1' se nao encontrar*/
@@ -364,7 +380,7 @@ int findDiscpline(char id, Disciplina disc[]) {
   int not_found = 1;
 
   for(int i = 0; i < 10 && not_found; i++) {
-    if(strcmp(id, disc[i].id) == 0) {
+    if(id == atoi(disc[i].id)) {
       return i;
     }
   }
