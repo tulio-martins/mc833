f2939d0913ad6fbdf7e9f3c1818263b3da4e8e4a
nathyharumi <nathalia.kuromiya@gmail.com>
2018-06-17 23:30:03 -0300

    testes corretos

--- projeto-3/entradas/create-tests.py
+++ projeto-3/entradas/create-tests.py
@@ -1,17 +1,17 @@
 disciplines = ('MC833', 'MC102', 'MC536', 'MC750', 'MC358', 'MC458', 'MC558', 'MC658', 'MC346', 'MC886')
 professors = ('edmundo', 'zanoni', 'bruno', 'baranauskas', 'rezende', 'lehilton', 'christiane', 'flavio', 'wainer', 'rocha')
 
-for i in range(0, 5):
+for i in range(0, 6):
     #lista todos os codigos
     if i == 0:
-        for j in range(0, 49):
+        for j in range(0, 50):
             f = open("%d.in" %j, "w")
             f.write( str(i+1) + "\n7\n")
             f.close
     #retorna ementa
     elif i == 1:
         k = -1
-        for j in range(50, 99):
+        for j in range(50, 100):
             f = open(str(j) + ".in", "w")
             if (j%5) == 0:
                 k = k + 1
@@ -20,7 +20,7 @@ for i in range(0, 5):
     #retorna informacoes 1 disciplina
     elif i == 2:
         k = -1
-        for j in range(100, 149):
+        for j in range(100, 150):
             f = open(str(j) + ".in", "w")
             if (j%5) == 0:
                 k = k + 1
@@ -28,14 +28,14 @@ for i in range(0, 5):
             f.close
     #todas as informacoes de todas
     elif i == 3:
-        for j in range(150, 199):
+        for j in range(150, 200):
             f = open(str(j) + ".in", "w")
             f.write( str(i+1) + "\n7\n")
             f.close
     #escreve comentario
     elif i == 4:
         k = -1
-        for j in range(200, 249):
+        for j in range(200, 250):
             f = open(str(j) + ".in", "w")
             if (j%5) == 0:
                 k = k + 1
@@ -44,7 +44,7 @@ for i in range(0, 5):
     #retorna comentario
     elif i == 5:
         k = -1
-        for j in range(250, 299):
+        for j in range(250, 300):
             f = open(str(j) + ".in", "w")
             if (j%5) == 0:
                 k = k + 1
